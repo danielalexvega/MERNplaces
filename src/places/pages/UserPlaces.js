c9145cd4c072d567ff874d42004745005d872bc5
Daniel Vega <danielalexvega@gmail.com>
2021-07-12 22:51:21 -0500

    work on modal component on placeitem page

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,37 +1,40 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 import PlaceList from '../components/PlaceList';
 
-const UserPlaces = () => {
-    const DUMMY_PLACES = [
-        {
-            id: 'p1',
-            title: 'Empire State Building',
-            description: 'One of the most famous sky scrapers in the world!',
-            imageUrl: 'https://cdn.britannica.com/73/114973-050-2DC46083/Midtown-Manhattan-Empire-State-Building-New-York.jpg',
-            address: '20 W 34th St, New York, NY 10001',
-            creator: 'u1',
-            location: {
-                lat: 40.748817,
-                long:-73.985428
-            } 
-        },
-        {
-            id: 'p2',
-            title: 'Empire State Building',
-            description: 'One of the most famous sky scrapers in the world!',
-            imageUrl: 'https://cdn.britannica.com/73/114973-050-2DC46083/Midtown-Manhattan-Empire-State-Building-New-York.jpg',
-            address: '20 W 34th St, New York, NY 10001',
-            creator: 'u2',
-            location: {
-                lat: 40.748817,
-                long:-73.985428
-            }
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world!',
+        imageUrl: 'https://cdn.britannica.com/73/114973-050-2DC46083/Midtown-Manhattan-Empire-State-Building-New-York.jpg',
+        address: '20 W 34th St, New York, NY 10001',
+        creator: 'u1',
+        location: {
+            lat: 40.748817,
+            long:-73.985428
+        } 
+    },
+    {
+        id: 'p2',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world!',
+        imageUrl: 'https://cdn.britannica.com/73/114973-050-2DC46083/Midtown-Manhattan-Empire-State-Building-New-York.jpg',
+        address: '20 W 34th St, New York, NY 10001',
+        creator: 'u2',
+        location: {
+            lat: 40.748817,
+            long:-73.985428
         }
-    ];
+    }
+];
 
+const UserPlaces = () => {
+    const userId = useParams().userId;
+    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
     return (
-        <PlaceList items={DUMMY_PLACES} />
+        <PlaceList items={loadedPlaces} />
     );
 }
 
