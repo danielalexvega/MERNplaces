17ab0435f3eb60ca0188a22d891936d871dbfe1f
Daniel Vega <danielalexvega@gmail.com>
2021-07-17 10:34:11 -0500

    working on validation for input component and state of the new place form

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -5,8 +5,8 @@ const Users = () => {
     const USERS = [
         {
             id: 'u1', 
-            name: 'Daniel Vega', 
-            image: 'https://scontent-dfw5-2.xx.fbcdn.net/v/t31.18172-8/416472_10100552736334047_1702655874_o.jpg?_nc_cat=102&ccb=1-3&_nc_sid=174925&_nc_ohc=UuR3I-lMDmQAX8Lpf0c&tn=RLI-rc0CqoanltZe&_nc_ht=scontent-dfw5-2.xx&oh=0bdc5e48cb9753e71890b8ff4392f520&oe=60EEEA3B', 
+            name: 'Nikola Juric', 
+            image: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80', 
             places: 3
         }
     ];
