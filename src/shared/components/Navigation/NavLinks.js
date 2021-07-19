435f4889b383a6a7105c5b19e816c774f30a112d
Daniel Vega <danielalexvega@gmail.com>
2021-07-18 22:56:18 -0500

    work on context

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,25 +1,40 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
 
-import './NavLinks.css';
+import { AuthContext } from "../../context/auth-context";
 
-const NavLinks = props => {
-    return (
-        <ul className='nav-links'>
-            <li>
-                <NavLink to="/" exact>ALL USERS</NavLink>
-            </li>
-            <li>
-                <NavLink to="/u1/places">MY PLACES</NavLink>
-            </li>
-            <li>
-                <NavLink to="/places/new">ADD PLACE</NavLink>
-            </li>
-            <li>
-                <NavLink to="/auth">LOG IN</NavLink>
-            </li>
-        </ul>
-    )
+import "./NavLinks.css";
+
+const NavLinks = (props) => {
+  const { isLoggedIn, logout} = useContext(AuthContext);
+
+  return (
+    <ul className="nav-links">
+      <li>
+        <NavLink to="/" exact>
+          ALL USERS
+        </NavLink>
+      </li>
+      {isLoggedIn && (
+        <li>
+          <NavLink to="/u1/places">MY PLACES</NavLink>
+        </li>
+      )}
+      {isLoggedIn && (
+        <li>
+          <NavLink to="/places/new">ADD PLACE</NavLink>
+        </li>
+      )}
+      {!isLoggedIn && <li>
+        <NavLink to="/auth">LOG IN</NavLink>
+      </li>}
+      {isLoggedIn && (
+          <li>
+              <button onClick={logout}>LOGOUT</button>
+          </li>
+      )}
+    </ul>
+  );
 };
 
 export default NavLinks;
---
+++ src/user/pages/Auth.js
@@ -0,0 +1,105 @@
+import React, { useState, useContext } from "react";
+
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import Card from "../../shared/components/UIElements/Card";
+import { useForm } from "../../shared/hooks/form-hook";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
+import { AuthContext } from "../../shared/context/auth-context";
+import "../../places/pages/PlaceForm.css";
+import "./Auth.css";
+
+const Auth = () => {
+  const {isLoggedIn, login, logout} = useContext(AuthContext);
+  const [isLoginMode, setIsLoginMode] = useState(true);
+
+  const [formState, inputHandler, setFormData] = useForm(
+    {
+      email: {
+        value: "",
+        isValid: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
+
+  const switchModeHandler = () => {
+    if (!isLoginMode) {
+      setFormData(
+        { ...formState.inputs, name: undefined },
+        formState.inputs.email.isValid && formState.inputs.password.isValid
+      );
+    } else {
+      setFormData({
+        ...formState.inputs,
+        name: {
+          value: "",
+          isValid: false,
+        },
+      });
+    }
+    setIsLoginMode((prevMode) => !prevMode);
+  };
+
+  const authSubmitHandler = (event) => {
+    event.preventDefault();
+
+    console.log(formState.inputs);
+    login();
+  };
+
+  return (
+    <Card className="authentication">
+      <h2>Login Required</h2>
+      <hr />
+
+      <form onSubmit={authSubmitHandler}>
+        {!isLoginMode && (
+          <Input
+            element="input"
+            id="name"
+            type="text"
+            label="Your Name"
+            validators={[VALIDATOR_REQUIRE]}
+            errorText="Please enter a name"
+            onInput={inputHandler}
+          />
+        )}
+        <Input
+          id="email"
+          element="input"
+          type="email"
+          label="Email Address"
+          errorText="Please enter a valid email address."
+          validators={[VALIDATOR_EMAIL]}
+          onInput={inputHandler}
+        />
+        <Input
+          id="password"
+          element="input"
+          type="password"
+          label="Password"
+          errorText="Please enter a valid password. (At least 5 characters)"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          onInput={inputHandler}
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          {isLoginMode ? "LOG IN" : "SIGN UP"}
+        </Button>
+      </form>
+      <Button inverse onClick={switchModeHandler}>
+        SWITCH TO {isLoginMode ? "SIGN UP" : "LOG IN"}
+      </Button>
+    </Card>
+  );
+};
+
+export default Auth;
